c7417b2f3edc9c4c062b757185cf2473d7a89859
Wesley Coutinho <142944005+wesley-coutinho@users.noreply.github.com>
2023-10-02 12:46:01 -0300

    processo de criação

---
+++ script.js
@@ -0,0 +1,4 @@
+const previa_operadores = document.querySelector("#previa_operadores");
+const operadores_atual = document.querySelector("#operadores_atual");
+const botao = document.querySelectorAll("#botoes button");
+
