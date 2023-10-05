26b070f574e304a150378659b7ca3df8e1a8fe0a
Wesley Coutinho <142944005+wesley-coutinho@users.noreply.github.com>
2023-10-05 18:22:39 -0300

    em processo de criação

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const previa_operadores = document.querySelector("#previa_operadores");
 const operadores_atual = document.querySelector("#operadores_atual");
-const botao = document.querySelectorAll("#botoes button");
+const botao = document.querySelectorAll("#botao button");
+
+/*class calculadora{
+    
+    
+}*/
+
+
+botao.forEach((btn) =>{
+    btn.addEventListener("click", (e) => {
+        const value = e.target.innerText;
+
+        console.log(value);
+    });
+});
 
